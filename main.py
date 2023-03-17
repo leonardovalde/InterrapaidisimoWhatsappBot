35d199bb3ac2d49be809ae13c1e7c4841e4c2c5f
Leonardo Valderrama <leonardovalde16@gmail.com>
2023-03-17 13:59:51 -0500

    finish main

--- main.py
+++ main.py
@@ -9,9 +9,18 @@ guide_number = properties.guide
 phone_number = properties.phoneNumber
 api_key = properties.apiKey
 
-status =  response = requests.post(url, json=data)
-    status_Code = response.status_code
+package_request =  response = requests.get(f'https://www3.interrapidisimo.com/ApiServInter/api/Mensajeria/ObtenerRastreoGuias?guias={guide_number}')
 
-whatsapp_api_url_request = f'https://api.callmebot.com/whatsapp.php?phone={phone_number}&text="+status+"&apikey={api_key}'
+json_data = (package_request.json()[0])["TrazaGuia"]
+guide_status = json_data["DescripcionEstadoGuia"]
+whatsapp_api_url_request = f'https://api.callmebot.com/whatsapp.php?phone={phone_number}&text="{guide_status}"&apikey={api_key}'
+requests.get(whatsapp_api_url_request)
 
-print(whatsapp_api_url_request)
+while True:
+    ackage_request =  response = requests.get(f'https://www3.interrapidisimo.com/ApiServInter/api/Mensajeria/ObtenerRastreoGuias?guias={guide_number}')
+    json_data = (package_request.json()[0])["TrazaGuia"]
+    guide_status_aux = json_data["DescripcionEstadoGuia"]
+    if guide_status != guide_status_aux:
+        guide_status = guide_status_aux
+        requests.get(whatsapp_api_url_request)
+    time.sleep(600)
